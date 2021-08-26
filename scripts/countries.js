a67a6d597eded0e11baec59bbdc3182682b7516e
Muhammed Fathah <fathah1@gmail.com>
2021-08-26 23:03:52 +0400

    now second page changes for each country

--- scripts/countries.js
+++ scripts/countries.js
@@ -1,44 +1,68 @@
+ function getValues(array,countryName){
+        let countryArray = array.filter(number =>number.name == countryName);
+        console.log(countryArray);
+        return countryArray;
+    }
+
+
 document.addEventListener("DOMContentLoaded", ()=>{ //Domcontentloaded checks(listens) if everything is loaded and then executes the code
 const selectDrop = document.querySelector('#countries');
 
-
-fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations').then(res=>{
+fetch('https://corona-api.com/countries').then(res=>{
     return res.json();
 }).then(data =>{
-    
-    console.log(data.locations)
     let output ="";
-    let sortedArray=data.locations.sort((a,b) => a.country < b.country ? -1 : a.country === b.country ? 0 : 1); //sorting the array 
+    console.log(data.data);
+    sortedArray=data.data.sort((a,b) => a.name < b.name ? -1 : a.name === b.name ? 0 : 1); //sorting the array 
     
     sortedArray.forEach(country => {
-       output += `<option value="${country.country}">${country.country}</option>`
+       output += `<option value="${country.name}">${country.name}</option>`
        
     });
-
     selectDrop.innerHTML = output;
+
+   
+    getValues(sortedArray,"Afghanistan")
+
 }).catch(err =>{
     console.log(err);
 })
 
 })
 
-
-    
 function updateData() {
-
     var select=document.getElementById("countries");
     var option = select.value;
-    document.getElementById("Heading1Page2").innerHTML= "Latest Stats for <br> " + option;
-    console.log(option);
 
+    countryData =getValues(sortedArray,option); //calls getValues everytime option is changed
 
+    console.log(Object.keys(countryData));
+    console.log(countryData[0]);
 
-
-
-}
+    var latestCases = countryData[0].today.confirmed;
+    var latestDeaths = countryData[0].today.deaths;
    
 
-  
+    var allTimeCases = countryData[0].latest_data.confirmed;
+    var allTimeDeaths = countryData[0].latest_data.deaths;
+    var allTimeRecovered = countryData[0].latest_data.recovered;
+
+    console.log(allTimeCases);
+
+   
+    document.getElementById("Heading1Page2").innerHTML= "Latest Stats for <br> " + option;
+    document.getElementById("latestCases").innerHTML = latestCases;
+    document.getElementById("latestDeaths").innerHTML = latestDeaths;
+    
+
+
+    document.getElementById("allTimeCases").innerHTML =  allTimeCases;
+    document.getElementById("allTimeDeaths").innerHTML = allTimeDeaths;
+    document.getElementById("allTimeRecovered").innerHTML = allTimeRecovered;
+}
+
+
+
 
 
 
