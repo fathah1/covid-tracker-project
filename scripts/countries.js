e9e81b2d4dea45dbea7398db558f47ddbfa49225
Muhammed Fathah <fathah1@gmail.com>
2021-08-26 05:12:22 +0400

    changed api since previous didnt have death and recover numbers

--- scripts/countries.js
+++ scripts/countries.js
@@ -1,19 +1,17 @@
-var select=document.getElementById("countries");
-
 document.addEventListener("DOMContentLoaded", ()=>{ //Domcontentloaded checks(listens) if everything is loaded and then executes the code
 const selectDrop = document.querySelector('#countries');
 
 
-fetch('https://api.covid19api.com/countries').then(res=>{
+fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations').then(res=>{
     return res.json();
 }).then(data =>{
     
-    
+    console.log(data.locations)
     let output ="";
-    let sortedArray=data.sort((a,b) => a.Country < b.Country ? -1 : a.Country === b.Country ? 0 : 1); //sorting the array 
+    let sortedArray=data.locations.sort((a,b) => a.country < b.country ? -1 : a.country === b.country ? 0 : 1); //sorting the array 
     
     sortedArray.forEach(country => {
-       output += `<option value="${country.Country}">${country.Country}</option>`
+       output += `<option value="${country.country}">${country.country}</option>`
        
     });
 
@@ -26,19 +24,22 @@ fetch('https://api.covid19api.com/countries').then(res=>{
 
 
     
-function updateHeading() {
+function updateData() {
 
     var select=document.getElementById("countries");
     var option = select.value;
-
     document.getElementById("Heading1Page2").innerHTML= "Latest Stats for <br> " + option;
-
     console.log(option);
+
+
+
+
 
 }
    
-updateHeading();
+
   
+
 
 
 
